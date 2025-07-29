8e0a0c8b25e096bedcc3787e6e09783cc7e1ea2a
Ali1Saleem1Hasan <ali1saleem1hasan@gmail.com>
2025-07-29 17:54:43 +0200

    Add Swagger API to the application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DataResponseInterceptor } from 'common/interceptors/data-response-interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Nowhere API')
+    .setDescription("Know what's happening near you!")
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   app.useGlobalInterceptors(new DataResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,17 +9,27 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({
+    summary: "Create a user (user's sign up)",
+    description:
+      'Sign up a new user to the application by save the infromation for later use...',
+  })
   createUser(@Body('user', ValidationPipe) createUserDto: CreateUserDTO) {
     this.usersService.createUser(createUserDto);
   }
 
   @Get(':email')
+  @ApiOperation({
+    description: 'Just for internal use to authenticate users by emails',
+  })
   getByEmail(@Param('email') email: string) {
     // TODO: add security on this param
     this.usersService.getUserByEmail(email);
