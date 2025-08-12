3b5e42373a4da747282937f1dcb964593512f89b
Ali1Saleem1Hasan <ali1saleem1hasan@gmail.com>
2025-08-12 11:10:03 +0200

    prevent user from posting more than one snap in one region

--- src/snaps/snaps.service.ts
+++ src/snaps/snaps.service.ts
@@ -1,10 +1,16 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateSnapDto } from './dto/create-snap.dto';
 import { Snap, SnapDocument } from './schemas/snap.schema';
 import { FilterQuery, Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { SnapsGetaway } from './getaway';
 import { handleMongoError } from 'common/utils/handle-mongoose-errors';
+import { ConfigService } from '@nestjs/config';
 // import { UpdateSnapDto } from './dto/update-snap.dto';
 
 @Injectable()
@@ -12,6 +18,7 @@ export class SnapsService {
   constructor(
     @InjectModel(Snap.name) private snapModel: Model<Snap>,
     private snapsGateaway: SnapsGetaway,
+    private configService: ConfigService,
   ) {}
 
   async create(
@@ -23,9 +30,17 @@ export class SnapsService {
     createSnapDto._userId = id;
     createSnapDto.snaps = snaps.map((snap) => snap.path);
 
+    // check if user has already posted a snap in the same area
+
     let location = createSnapDto.location;
     if (typeof location === 'string') location = JSON.parse(location);
     createSnapDto.location = location;
+
+    const alreadyHasPostedNear = await this.findNear(location.coordinates, id);
+    if (alreadyHasPostedNear.length > 0)
+      throw new ForbiddenException(
+        'User has already posted in this area today!!',
+      );
 
     try {
       // first save the data
@@ -46,10 +61,12 @@ export class SnapsService {
 
   async findNear(
     location: [number, number],
-
-    maxDistanceInMeters: number = 20000,
+    _userId?: string,
+    maxDistanceInMeters: number = this.configService.get('MAX_DISTANCE_NEAR') ||
+      20000,
   ) {
     return await this.snapModel.find({
+      ...(_userId && { _userId }),
       location: {
         $near: {
           $geometry: {
