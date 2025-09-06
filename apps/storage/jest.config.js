d3229e8c163fce857fac7b27c7fa4edecede34b1
Ali1Saleem1Hasan <ali1saleem1hasan@gmail.com>
2025-09-06 11:49:22 +0200

    configure tests for apps with pnpm

---
+++ apps/storage/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('jest').Config} */
+const base = require('../../jest.config.base');
+
+module.exports = {
+  ...base,
+  rootDir: 'src',
+  displayName: 'storage',
+};
---
+++ jest.config.base.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
