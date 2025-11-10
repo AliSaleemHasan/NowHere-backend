f5fd70655a61caf394bf6b020a31f50ac7f5c7a3
Ali1Saleem1Hasan <ali1saleem1hasan@gmail.com>
2025-11-10 22:32:13 +0100

    configure aws-sdk correctly to work with minio

--- apps/storage/src/s3ObjectProvider/s3-object.provider.ts
+++ apps/storage/src/s3ObjectProvider/s3-object.provider.ts
@@ -11,11 +11,16 @@ export class S3Provider {
     this.bucket = this.config.get<string>('AWS_BUCKET', '');
 
     this.client = new S3Client({
+      endpoint: this.config.get<string>(
+        'AWS_ENDPOINT_URL',
+        'http://minio-local:9000',
+      ),
       region: this.config.get<string>('AWS_REGION', 'us-east-1'),
       credentials: {
         accessKeyId: this.config.get<string>('AWS_ACCESS_KEY_ID', ''),
         secretAccessKey: this.config.get<string>('AWS_SECRET_ACCESS_KEY', ''),
       },
+      forcePathStyle: true,
     });
   }
 
